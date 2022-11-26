50b6c9fd6aee1a37ae792aab013398e4ac23015d
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-11-26 11:21:48 +0200

    add posts

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+    #validate if title. description is empty
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
+end
