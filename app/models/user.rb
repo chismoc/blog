2260af1b299f29f7c5ff648f3631700b409b28e2
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-12-12 22:45:38 +0200

    add profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
          has_many :posts, dependent: :destroy
          has_many :comments, dependent: :destroy
          has_many :notifications, as: :recipient, dependent: :destroy
-
+         
+         has_one_attached :avatar
+         
          has_one :address, dependent: :destroy, inverse_of: :user, autosave: true
          enum role: [:user, :admin]
          after_initialize :set_default_role, if: :new_record?
